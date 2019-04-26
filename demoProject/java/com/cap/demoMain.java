8b47271030007ef4c62986a9e96143ab1a887fed
snmanga <snmanga@DIN69001048.corp.capgemini.com>
2019-04-26 16:01:36 +0530

    "second"

--- demoProject/java/com/cap/demoMain.java
+++ demoProject/java/com/cap/demoMain.java
@@ -2,7 +2,7 @@ package com.cap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Bindusri kongara
 @SpringBootApplication
 public class demoMain {
 	public static void main(String[] args) {
